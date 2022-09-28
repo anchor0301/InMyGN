269f239225215355cc562a6fb530e9bda18dc2a3
min <wp2102@kakao.com>
2022-09-28 15:17:07 +0900

    map 구현
    (속도가 너무 느림)

--- app/src/main/java/com/example/inmygn/mapSeason.java
+++ app/src/main/java/com/example/inmygn/mapSeason.java
@@ -14,10 +14,12 @@ import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -46,7 +48,7 @@ public class mapSeason extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     ArrayAdapter adapter;
 
-    String urlAddress = address + "?serviceKey=" + key + "&pageNo=1&numOfRows=55&resultType=json" ;
+    String urlAddress = address + "?serviceKey=" + key + "&pageNo=1&numOfRows=55&resultType=json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,70 +69,77 @@ public class mapSeason extends AppCompatActivity implements OnMapReadyCallback {
         Context context = this;
         // final LatLng Pocheon = new LatLng(37.894936, 127.200344);   // 마커 추가
 
-                try {
-                    URL url = new URL(urlAddress);
-                    InputStream is =  url.openStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader reader = new BufferedReader(isr);
+        try {
+            URL url = new URL(urlAddress);
+            InputStream is = url.openStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader reader = new BufferedReader(isr);
 
-                    StringBuffer buffer = new StringBuffer();
-                    String line = reader.readLine();
-                    while (line != null) {
-                        buffer.append(line + "\n");
-                        line = reader.readLine();
-                    }
+            StringBuffer buffer = new StringBuffer();
+            String line = reader.readLine();
+            while (line != null) {
+                buffer.append(line + "\n");
+                line = reader.readLine();
+            }
 
-                    String jsonData = buffer.toString();
+            String jsonData = buffer.toString();
 
-                    // jsonData를 먼저 JSONObject 형태로 바꾼다.
-                    JSONObject obj = new JSONObject(jsonData);
+            // jsonData를 먼저 JSONObject 형태로 바꾼다.
+            JSONObject obj = new JSONObject(jsonData);
 
-                    // obj의 "gyeongnamtourseasonlist"의 JSONObject를 추출
-                    JSONObject gyeongnamtourseasonlist = (JSONObject)obj.get("gyeongnamtourseasonlist");
-
-
-                    // gyeongnamtourseasonlist "body"의 JSONObject를 추출
-                    JSONObject SeasonBody = (JSONObject)gyeongnamtourseasonlist.get("body");
-
-                    // SeasonBody "item"의 JSONObject를 추출
-                    JSONObject SeasonItems = (JSONObject)SeasonBody.get("items");
+            // obj의 "gyeongnamtourseasonlist"의 JSONObject를 추출
+            JSONObject gyeongnamtourseasonlist = (JSONObject) obj.get("gyeongnamtourseasonlist");
 
 
+            // gyeongnamtourseasonlist "body"의 JSONObject를 추출
+            JSONObject SeasonBody = (JSONObject) gyeongnamtourseasonlist.get("body");
 
-                    // boxOfficeResult의 JSONObject에서 "dailyBoxOfficeList"의 JSONArray 추출
-                    JSONArray dailyBoxOfficeList = (JSONArray)SeasonItems.get("item");
-
-                    for (int i = 0; i < dailyBoxOfficeList.length(); i++) {
-
-                        JSONObject temp = dailyBoxOfficeList.getJSONObject(i);
-
-                        //타이틀 제목
-                        String data_title = temp.getString("user_address");
+            // SeasonBody "item"의 JSONObject를 추출
+            JSONObject SeasonItems = (JSONObject) SeasonBody.get("items");
 
 
-                        Location cityHallLocation = addrToPoint(context,data_title);
-                        final LatLng Pocheon = new LatLng(cityHallLocation.getLatitude(), cityHallLocation.getLongitude());
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(Pocheon);
-                        markerOptions.title(data_title);                                    // 마커 옵션 추가
-                        googleMap.addMarker(markerOptions);                                 // 마커 등록
-                    }
+            // boxOfficeResult의 JSONObject에서 "dailyBoxOfficeList"의 JSONArray 추출
+            JSONArray dailyBoxOfficeList = (JSONArray) SeasonItems.get("item");
 
+            for (int i = 0; i < dailyBoxOfficeList.length(); i++) {
 
+                JSONObject temp = dailyBoxOfficeList.getJSONObject(i);
 
+                //타이틀 제목
+                String data_title = temp.getString("user_address");
+                String data_content = temp.getString("data_content");
 
+                //문자열 자르기
 
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                // '/'를 기준으로 문자열을 자른다.
+                String date[] = data_title.split("/");
+
+                for (int j = 0; j < date.length; j++) {
+                    Location cityHallLocation = addrToPoint(context, date[j]);
+                    LatLng Pocheon = new LatLng(cityHallLocation.getLatitude(), cityHallLocation.getLongitude());
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(Pocheon)
+                            .title(date[j])                                    // 마커 옵션 추가
+                            .snippet(data_content);                    //부가 설명
+                    googleMap.addMarker(markerOptions);                                 // 마커 등록
                 }
+
+
+            }
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.4605301, 128.2131958), 9));
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);     // 지도 유형 설정
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(YN, 13));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public static Location addrToPoint(Context context,String locationName) {
+    public static Location addrToPoint(Context context, String locationName) {
         Location location = new Location("");
         Geocoder geocoder = new Geocoder(context);
         List<Address> addresses = null;
